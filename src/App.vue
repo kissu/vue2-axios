16abf9e5a943bc6a9995028dcd17bab3f2481084
Konstantin BIFERT <konstantin.bifert@gmail.com>
2022-05-05 17:24:45 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <pre v-if="todo">{{ todo }}</pre>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      todo: false,
+    };
+  },
+  async created() {
+    try {
+      const response = await this.axios(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      );
+
+      console.log(response);
+      this.todo = response.data;
+    } catch (error) {
+      console.error("error >>", error);
+    }
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
